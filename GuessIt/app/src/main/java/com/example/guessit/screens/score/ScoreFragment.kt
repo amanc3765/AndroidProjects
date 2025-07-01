dc8b55148e0697084a48cf5f09079a7a87ba5bc8
Aman Choudhary <amanc3765@gmail.com>
2025-07-01 11:08:39 +0530

    Initial commit

---
+++ GuessIt/app/src/main/java/com/example/guessit/screens/score/ScoreFragment.kt
@@ -0,0 +1,36 @@
+package com.example.guessit.screens.score
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guessit.R
+import androidx.databinding.DataBindingUtil
+import com.example.guessit.databinding.ScoreFragmentBinding
+
+
+class ScoreFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+
+        // Inflate view and obtain an instance of the binding class.
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.score_fragment, container, false
+        )
+
+        // Get args using by navArgs property delegate
+//        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+//        binding.scoreText.text = scoreFragmentArgs.score.toString()
+//        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        return binding.root
+    }
+
+    private fun onPlayAgain() {
+//        findNavController().navigate(ScoreFragmentDirections.actionRestart())
+    }
+
+}
---
+++ GuessIt/app/src/main/java/com/example/guessit/screens/title/TitleFragment.kt
@@ -0,0 +1,29 @@
+package com.example.guessit.screens.title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guessit.R
+import androidx.databinding.DataBindingUtil
+import com.example.guessit.databinding.TitleFragmentBinding
+import androidx.navigation.fragment.findNavController
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        val binding: TitleFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.title_fragment, container, false
+        )
+
+        binding.playGameButton.setOnClickListener {
+//            findNavController().navigate(TitleFragmentDirections.actionTitleToGame())
+        }
+
+        return binding.root
+    }
+
+}
