391aeac53203590ecaca72c4fb731bd8384eda38
Aman Choudhary <amanc3765@gmail.com>
2025-07-06 01:04:51 +0530

    Added UI for ChatScreen

--- SocialMedia/app/build.gradle.kts
+++ SocialMedia/app/build.gradle.kts
@@ -43,6 +43,8 @@ dependencies {
     implementation(libs.coil)
     implementation(libs.coil.compose)
 
+    implementation(libs.compose.material.icons)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
